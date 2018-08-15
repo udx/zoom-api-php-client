2cce8f130e680b4e49fbf4d922db7fc98c065131
Anton <antonkorotkoff@gmail.com>
2018-08-15 17:11:15 +0300

    Meetings endpoint tests

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -30,10 +30,28 @@ class Bootstrap extends TestCase {
             $client = new Zoom\Zoom( 'key', 'secret' );
             $this->assertInstanceOf(Zoom\Endpoint\Meetings::class, $client->meetings);
             $this->assertInstanceOf(Zoom\Http\Request::class, $client->meetings);
-            $this->assertTrue( method_exists( $client->meetings, 'list' ) );
-            $this->assertTrue( method_exists( $client->meetings, 'create' ) );
-            $this->assertTrue( method_exists( $client->meetings, 'meeting' ) );
-            $this->assertTrue( method_exists( $client->meetings, 'records' ) );
+            $this->assertTrue( method_exists( $client->meetings, 'list' ),
+                "'list' method is missing" );
+            $this->assertTrue( method_exists( $client->meetings, 'create' ),
+                "'create' method is missing" );
+            $this->assertTrue( method_exists( $client->meetings, 'meeting' ),
+                "'meeting' method is missing" );
+            $this->assertTrue( method_exists( $client->meetings, 'remove' ),
+                "'remove' method is missing" );
+            $this->assertTrue( method_exists( $client->meetings, 'update' ),
+                "'update' method is missing" );
+            $this->assertTrue( method_exists( $client->meetings, 'status' ),
+                "'status' method is missing" );
+            $this->assertTrue( method_exists( $client->meetings, 'listRegistrants' ),
+                "'listRegistrants' method is missing" );
+            $this->assertTrue( method_exists( $client->meetings, 'addRegistrant' ),
+                "'addRegistrant' method is missing" );
+            $this->assertTrue( method_exists( $client->meetings, 'updateRegistrantStatus' ),
+                "'updateRegistrantStatus' method is missing" );
+            $this->assertTrue( method_exists( $client->meetings, 'pastMeeting' ),
+                "'pastMeeting' method is missing" );
+            $this->assertTrue( method_exists( $client->meetings, 'pastMeetingParticipants' ),
+                "'pastMeetingParticipants' method is missing" );
         } catch ( Exception $e ) {
             $this->fail( $e );
         }
